cde15d38b354ef26e6e1361c1454c1171dc27464
Zebulun Arendsee <zbwrnz@gmail.com>
2021-04-19 09:19:53 -0500

    Render tesselations as simple angle paths
    A tesselation can be represented as a tree of angles. Each node contains
    an angle relative to its parent. Each child is a connected vertex.
    
    My current implementation is 1) incorrect (the angles are off so the
    whole graph is heavily disjoined) and 2) exponential (there is no
    bounding so each vertex is represented by an exponential number of
    nodes.
    
    So ... it needs amendments.

--- src/Tesselation.hs
+++ src/Tesselation.hs
@@ -6,11 +6,56 @@ module Tesselation (myFigs) where
 
 import Diagrams.Prelude
 import qualified Diagrams.Backend.SVG.CmdLine as S
+import qualified Diagrams.TwoD.Vector as TwoD
 
-type Point' = (Double, Double)
+data RoseTree a = RoseTree a [RoseTree a]
+
+-- counvert a double to a large int that is used to check for node existence in
+-- maps. The original doubles cannot be used because imprecision screw equality.
+preciseHash :: Double -> Int
+preciseHash x = round(x * 2^32)
 
 ngon :: Int ->  Diagram S.B
 ngon i = regPoly i 1 # lw veryThin
+
+renderPath :: [Double] -> Diagram S.B
+renderPath = lw veryThin . strokeLocTrail . fromOffsets . map (\x -> TwoD.e (x @@ rad))
+
+angleStroke :: Double -> Diagram S.B
+angleStroke a = fromOffsets [TwoD.e (a @@ rad)] # lw veryThin
+
+renderAngleTree :: RoseTree Double -> Diagram S.B
+renderAngleTree (RoseTree _ xs) = mconcat [beside (r2 (cos a, sin a)) (angleStroke a) (renderAngleTree x) | x@(RoseTree a _) <- xs]
+
+-- This first draft of the tree maker does not ensure paths are unique, so the
+-- tree grows exponentially. Every node in the tree Every node in the tree
+-- stores an angle, which is the edge from its parent. To build the tree, draw
+-- lines of unit length from root rotating by the given angle as you descend
+-- through the children.
+badTileTree
+  :: Int   -- tree depth
+  -> [Int] -- tiling pattern (e.g., 3.4.6.4)
+  -> RoseTree Double -- this vertex
+badTileTree = makeBadTileTree 0 where
+  makeBadTileTree
+    :: Double -- angle
+    -> Int -- depth
+    -> [Int] -- the set clock
+    -> RoseTree Double
+  makeBadTileTree angle d clock
+    | d <= 0 = RoseTree angle []
+    | otherwise = RoseTree angle
+        [
+          makeBadTileTree
+            (newAngle angle (take (i+1) clock))  -- calculate the child angle relative to parent 
+            (d-1)  -- one step closer to the end
+            (drop i clock ++ take i clock)  -- shift the clock for the child
+        | i <- [0 .. length clock - 1]
+        ]
+
+  newAngle :: Double -> [Int] -> Double
+  newAngle angle clock = sum (map innerAngle clock)
+
 
 -- minimal distance from origin to a face
 faceDistance :: Int -> Double
@@ -59,21 +104,18 @@ myFigs = foldl (beside (r2 (0,-1))) mempty
  , atPoints (trailVertices $ regPoly 6 1) (repeat (circle 0.2 # fc blue)) <> ngon 6
  -- , foldl (<>) (ngon 6) [regularOffset i 6 4 (ngon 4) | i <- [1..6]]
  , ngon 6 <> circle 0.3 # fc grey <>
-     foldl (<>) mempty [regularOffset i 6 4 (text (show i) # fontSizeL 0.2 <> ngon 4 # fc blue) | i <- [0..5]]
- , foldl (<>) mempty [(ngon n # fc green) | (i,n) <- (zip [1..10] [3..12])]
+     mconcat [regularOffset i 6 4 (text (show i) # fontSizeL 0.2 <> ngon 4 # fc blue) | i <- [0..5]]
+ , mconcat [(ngon n # fc green) | (i,n) <- (zip [1..10] [3..12])]
  , foldl (|||) mempty [(ngon n # fc green) | (i,n) <- (zip [1..10] [3..12])]
  , ngon 6 <> circle 0.3 # fc grey <>
-     foldl (<>) mempty [regularOffset i 6 n (text (show i) # fontSizeL 0.5 <> ngon n) | (i,n) <- (zip [0..5] [3..8])]
+     mconcat [regularOffset i 6 n (text (show i) # fontSizeL 0.5 <> ngon n) | (i,n) <- (zip [0..5] [3..8])]
  , ngon 4 # rotate (15 @@ deg) <>
-     foldl (<>) mempty [regularOffset i 4 n (text (show i) # fontSizeL 0.5 <> ngon n) | (i,n) <- (zip [0..3] [3..6])] # rotate (15 @@ deg)
- , foldl (<>) mempty [(cornerPoly n # fc green) | (i,n) <- (zip [1..10] [3..12])]
+     mconcat [regularOffset i 4 n (text (show i) # fontSizeL 0.5 <> ngon n) | (i,n) <- (zip [0..3] [3..6])] # rotate (15 @@ deg)
+ , mconcat [(cornerPoly n # fc green) | (i,n) <- (zip [1..10] [3..12])]
  , regCycle [3,4,6,4]
  , regCycle [3,3,3,4,4]
  , regCycle [12,3,12]
+ , lw veryThin . strokeLocTrail . fromOffsets $ [TwoD.e (60 @@ deg), TwoD.e (30 @@ deg)]
+ , renderAngleTree $ badTileTree 3 [3,4,6,4]
+ , renderAngleTree $ badTileTree 3 [3,3,3,4,4]
  ]
-
-surround :: Diagram S.B
-surround = undefined
-
-regular :: Double -> Int -> [Int] -> [(Point', Point')]
-regular _ _ _ = []
