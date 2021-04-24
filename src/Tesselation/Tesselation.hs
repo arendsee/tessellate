7556dffddfe03a7052d33d67585b9fd6ef6c69e3
Zebulun Arendsee <zbwrnz@gmail.com>
2021-04-24 00:02:58 -0500

    Less the repeats, no less the bug

--- src/Tesselation/Tesselation.hs
+++ src/Tesselation/Tesselation.hs
@@ -12,6 +12,28 @@ import qualified Control.Monad.State as CMS
 
 type TilingState = CMS.State (Set.Set (Int, Int))
 
+-- get the unit location and hash of a child node a give angle from the parent
+childLocation :: Double -> (Double, Double) -> ((Double, Double), (Int, Int))
+childLocation a (x,y) = ((x', y'), idx) where
+  (x',y') = (x + cos a, y + sin a)
+  idx = (preciseHash x', preciseHash y')
+
+toChild
+  ::  (Double, (Double, Double), [Int]) -> Int -> Int
+  -> ((Double, (Double, Double), [Int]), (Int, Int))
+toChild (a, p, c) d i = ((a', p', c'), idx) where
+  a' = a + pi + (if mod d 2 == 0 then -1 else 1) * sum (map innerAngle (take i c))
+  c' = drop i c ++ take i c
+  (p', idx) = childLocation a' p
+
+
+toChildClockwise
+  ::  (Double, (Double, Double), [Int]) -> Int -> Int
+  -> ((Double, (Double, Double), [Int]), (Int, Int))
+toChildClockwise (a, p, c) _ i = ((a', p', c'), idx) where
+  a' = a + pi + sum (map innerAngle (take i c))
+  c' = reverse (take i c) ++ reverse (drop i c)
+  (p', idx) = childLocation a' p
 
 regTessBreadthMaze
   :: Int -- depth
@@ -32,17 +54,14 @@ regTessBreadthMaze d0 clock
     | otherwise = error "The algorithm is broken"
 
   step :: (Double, (Double, Double), [Int]) -> Int -> Int -> TilingState (Maybe (RoseTree (Double, (Double, Double), [Int]))) 
-  step (a, (x,y), c) d i = do
+  step x d i = do
       s <- CMS.get
-      let a' = a + pi + (if mod d 2 == 0 then -1 else 1) * sum (map innerAngle (take i c))
-          c' = drop i c ++ take i c
-          (x',y') = (x + cos a', y + sin a')
-          idx = (preciseHash x', preciseHash y')
+      let (x', idx) = toChild x d i
       if Set.member idx s
         then return Nothing
         else do
           CMS.put (Set.insert idx s)
-          return . Just $ RoseTree (a', (x',y'), c') []
+          return . Just $ RoseTree x' []
 
 
 regTessBreadthTree
@@ -58,25 +77,21 @@ regTessBreadthTree d0 c0
     -> Int -- depth for this iteration
     -> RoseTree (Double, (Double, Double), [Int])
     -> TilingState (RoseTree (Double, (Double, Double), [Int]))
-  f dcur dmax (RoseTree x@(_, (px, py), _) xs)
+  f dcur dmax (RoseTree x xs)
     | dcur < dmax = RoseTree x <$> mapM (f (dcur + 1) dmax) xs
     | dcur == dmax = RoseTree x <$> mapM (step x dcur) [0 .. length c0 - 1]
     | otherwise = error "The algorithm is broken"
   f _ _ x@(RoseLeaf _) = return x
 
   step :: (Double, (Double, Double), [Int]) -> Int -> Int -> TilingState (RoseTree (Double, (Double, Double), [Int]))
-  step (a, (x,y), c) d i = do
+  step x d i = do
       s <- CMS.get
-      -- let a' = a + pi + sum (map innerAngle (take i c))
-      let a' = a + pi - (if mod d 2 == 0 then -1 else 1) * sum (map innerAngle (take i c))
-          c' = drop i c ++ take i c
-          (x',y') = (x + cos a', y + sin a')
-          idx = (preciseHash x', preciseHash y')
+      let (x', idx) = toChild x d i 
       if Set.member idx s
-        then return (RoseLeaf (a', (x',y'), c'))
+        then return (RoseLeaf x')
         else do
             CMS.put (Set.insert idx s)
-            return $ RoseTree (a', (x',y'), c') []
+            return $ RoseTree x' []
 
 
 regTessMaze
@@ -95,19 +110,17 @@ regTessMaze alt depth0 clock = fst $ CMS.runState (sTileTree (0,0) 0 depth0 cloc
     | d <= 0 = return $ RoseTree a []
     | otherwise = RoseTree a <$> fmap catMaybes (mapM (step p a d c) [0 .. length c - 1])
 
+  toChild' = if alt then toChild else toChildClockwise
+
   step :: (Double, Double) -> Double -> Int -> [Int] -> Int -> TilingState (Maybe (RoseTree Double)) 
-  step (x,y) a d c i = do
+  step p a d c i = do
       s <- CMS.get
-      let a' = a + pi + (if mod d 2 == 1 && alt then 1 else -1) * sum (map innerAngle (take i c))
-          c' = if alt then drop i c ++ take i c else reverse (take i c) ++ reverse (drop i c)
-          (x',y') = (x + cos a', y + sin a')
-          idx = (preciseHash x', preciseHash y')
+      let ((a', p', c'), idx) = toChild' (a, p, c) d i
       if Set.member idx s
         then return Nothing
         else do
           CMS.put (Set.insert idx s)
-          t <- sTileTree (x',y') a' (d-1) c'
-          return (Just t)
+          Just <$> sTileTree p' a' (d-1) c'
 
 myFigs = vcat
  [ renderAngleTree ( regTessMaze True 40 [4,4,4,4]   ) ||| renderAngleTree ( regTessMaze False 40 [4,4,4,4]   ) |||renderAngleTree ( regTessBreadthTree 12 [4,4,4,4]   ) ||| renderAngleTree ( regTessBreadthMaze 12 [4,4,4,4]   )
@@ -120,5 +133,7 @@ myFigs = vcat
  , renderAngleTree ( regTessMaze True 40 [3,3,3,3,6] ) ||| renderAngleTree ( regTessMaze False 40 [3,3,3,3,6] ) |||renderAngleTree ( regTessBreadthTree 12 [3,3,3,3,6] ) ||| renderAngleTree ( regTessBreadthMaze 12 [3,3,3,3,6] )
  , renderAngleTree ( regTessMaze True 25 [3,3,4,3,4] ) ||| renderAngleTree ( regTessMaze False 25 [3,3,4,3,4] ) |||renderAngleTree ( regTessBreadthTree 6  [3,3,4,3,4] ) ||| renderAngleTree ( regTessBreadthMaze 6  [3,3,4,3,4] )
  , renderAngleTree ( regTessMaze True 25 [3,3,3,4,4] ) ||| renderAngleTree ( regTessMaze False 25 [3,3,3,4,4] ) |||renderAngleTree ( regTessBreadthTree 6  [3,3,3,4,4] ) ||| renderAngleTree ( regTessBreadthMaze 6  [3,3,3,4,4] )
+ -- [ renderAngleTree ( regTessBreadthTree 1 [3,3,3,4,4]   ) # showOrigin
+ -- , hrule 10 # fc blue
+ -- , renderAngleTree ( regTessBreadthMaze 1 [3,3,3,4,4]   ) # showOrigin
  ]
-
